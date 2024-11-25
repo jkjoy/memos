805e9c2cd807ead10b6dd298408eaf94c389f0e1
浪子 <jkjoy@live.cn>
2024-11-25 09:02:49 +0800

    Update memos2.js

--- js/memos2.js
+++ js/memos2.js
@@ -97,11 +97,12 @@ window.onload = function() {
             // 创建 memo HTML 字符串，包括图片和内容
             htmlString += `
     <article class="card-wrapper card">
-        <div class="col-md-12">
+       <div class="post-preview row g-0 flex-md-row-reverse">
+        <div class="col-md-12 ">
           <div class="card-body d-flex flex-column">
             <div class="card-text content mt-0 mb-3">
               <p> ${processedContent}</p> 
-               <div class="${gridClass}">
+               <div class="inner ${gridClass}">
                               ${resourceElement}     
                         </div>
                     </div>    
@@ -118,7 +119,7 @@ ${new Date(createTime).toLocaleString()}
               </div>
             </div>
           </div>
-        </div>
+        </div></div>
             </article>`;
         });
         return htmlString;
